f8935237c6b923019a82b14047ef507b0f72b7e0
Brandon Brown <Brandon.Brown@metova.com>
2018-10-10 13:37:52 -0500

    add header, create post, and post list component

---
+++ src/app/posts/post-list/post-list.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-post-list',
+  templateUrl: './post-list.component.html',
+  styleUrls: ['./post-list.component.scss']
+})
+
+export class PostListComponent {
+  // posts = [
+  //   { title: 'First post', content: 'content section' },
+  //   { title: 'Second post', content: 'content section' },
+  //   { title: 'Third post', content: 'content section' }
+  // ];
+
+  @Input() posts = [];
+}
